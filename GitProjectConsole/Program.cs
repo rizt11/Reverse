c4300e80197c926bfc1ed8b6154df96ba72d909e
Aprizal <rizt11@yahoo.com>
2016-03-03 10:38:45 +0700

    Add comment to ReverseString

--- GitProjectConsole/Program.cs
+++ GitProjectConsole/Program.cs
@@ -12,6 +12,11 @@ namespace GitProjectConsole
         {
         }
 
+        /// <summary>
+        /// This is a string reverse
+        /// </summary>
+        /// <param name="input">Input string</param>
+        /// <returns></returns>
         public static string ReverseString(string input)
         {
             return new string(input.ToCharArray().Reverse().ToArray());
