4a2594b8b2d03c18163881d4cdc67685096d6551
Aprizal <rizt11@yahoo.com>
2016-03-03 10:27:55 +0700

    First Upload

---
+++ GitProjectConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitProjectConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string ReverseString(string input)
+        {
+            return new string(input.ToCharArray().Reverse().ToArray());
+        }
+    }
+}
